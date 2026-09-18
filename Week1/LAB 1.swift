e60f5f796cab9e7f96df5403c03344d734ef1271
Trầ Tuấn <nguyentrananhtuan1611@gmail.com>
2026-09-18 15:46:00 +0700

    Update LAB 1.swift

--- Week1/LAB 1.swift
+++ Week1/LAB 1.swift
@@ -1,1 +1,22 @@
+import SwiftUI
 
+let studentName: "Yuha"
+let studentID: "SESEIU24030"
+var age: 19
+var gpa: 3.0
+let isStudent: true 
+
+struct ContentView: View{
+  var body: some View{
+    VStack(spacing:12){
+      Text("Student Profile")
+        .font(.largeTitle)
+        .bold()
+      Text("Name: \(studentName)")
+      Text("Student ID: \(student ID)")
+      Text("Age: \(age)")
+      Text("GPA: \(gpa)")
+      Text("Student: \(isStudent)")
+    }
+  }
+}
